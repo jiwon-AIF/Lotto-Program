bd3617a8106ee25fc2f19c618693f95b5838e8ff
Jiwon <jwj50364@gmail.com>
2020-04-09 14:47:38 +0900

    lotto_program(rev)
    랜덤으로 출력되는 로또 번호 6개와 입력 번호 6개 값 비교

--- lotto.go
+++ lotto.go
@@ -11,6 +11,7 @@ import (
 func main() {
 
 	for {
+
 		reader := bufio.NewReader(os.Stdin)
 		choice, _ := reader.ReadString('\n')
 		choice = strings.TrimSpace(choice)
@@ -32,11 +33,31 @@ func main() {
 			for k := 0; k <= 5; k++ { // 채워진 배열을 출력하기 위한 for문
 				fmt.Println(a[k])
 				if k == 5 {
+					fmt.Println("입니다.")
 					break
 				}
+				var n1, n2, n3, n4, n5, n6 int
+				n1 = 1
+				n2 = 2
+				n3 = 3
+				n4 = 4
+				n5 = 5
+				n6 = 6
+
+				var r1, r2, r3, r4, r5, r6 int
+				r1 = a[k]
+				r2 = a[k]
+				r3 = a[k]
+				r4 = a[k]
+				r5 = a[k]
+				r6 = a[k]
+				fmt.Println(n1 == r1)
+				fmt.Println(n2 == r2)
+				fmt.Println(n3 == r3)
+				fmt.Println(n4 == r4)
+				fmt.Println(n5 == r5)
+				fmt.Println(n6 == r6)
 			}
-		} else if choice == "Q" {
-			os.Exit(1)
 		}
 	}
 }
